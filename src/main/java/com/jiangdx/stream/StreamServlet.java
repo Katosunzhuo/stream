3ccef273de54d227cafcc963ace7616f4e9904df
jiangdx <rain_man_happy@126.com>
2013-08-04 23:05:48 +0800

    maven

---
+++ src/main/java/com/jiangdx/stream/StreamServlet.java
@@ -0,0 +1,38 @@
+package com.jiangdx.stream;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * File reserved servlet, mainly reading the request parameter and its file
+ * part, stored it.
+ */
+public class StreamServlet extends HttpServlet {
+	private static final long serialVersionUID = -8619685235661387895L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.service(req, resp);
+	}
+
+}
