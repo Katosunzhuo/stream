24b495d7cb662ff4a3e33f248d9fec85b8f1cd24
jiangdx <rain_man_happy@126.com>
2013-08-04 23:25:24 +0800

    initialize the servlet

--- src/main/java/com/jiangdx/stream/StreamServlet.java
+++ src/main/java/com/jiangdx/stream/StreamServlet.java
@@ -1,31 +1,89 @@
 package com.jiangdx.stream;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 /**
  * File reserved servlet, mainly reading the request parameter and its file
  * part, stored it.
  */
+@WebServlet(name = "FileUploadServlet", urlPatterns = { "/upload" })
+@MultipartConfig
 public class StreamServlet extends HttpServlet {
 	private static final long serialVersionUID = -8619685235661387895L;
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+	protected void doPost(HttpServletRequest req, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doGet(req, resp);
+		response.setContentType("text/html;charset=UTF-8");
+
+		// Create path components to save the file
+		final String path = req.getParameter("destination");
+		final Part filePart = req.getPart("file");
+		final String fileName = getFileName(filePart);
+
+		OutputStream out = null;
+		InputStream filecontent = null;
+		final PrintWriter writer = response.getWriter();
+
+		try {
+			out = new FileOutputStream(new File(path + File.separator
+					+ fileName));
+			filecontent = filePart.getInputStream();
+
+			int read = 0;
+			final byte[] bytes = new byte[1024];
+
+			while ((read = filecontent.read(bytes)) != -1) {
+				out.write(bytes, 0, read);
+			}
+			writer.println("New file " + fileName + " created at " + path);
+//			LOGGER.log(Level.INFO, "File{0}being uploaded to {1}",
+//					new Object[] { fileName, path });
+		} catch (FileNotFoundException fne) {
+			writer.println("You either did not specify a file to upload or are "
+					+ "trying to upload a file to a protected or nonexistent "
+					+ "location.");
+			writer.println("<br/> ERROR: " + fne.getMessage());
+
+//			LOGGER.log(Level.SEVERE, "Problems during file upload. Error: {0}",
+//					new Object[] { fne.getMessage() });
+		} finally {
+			if (out != null) {
+				out.close();
+			}
+			if (filecontent != null) {
+				filecontent.close();
+			}
+			if (writer != null) {
+				writer.close();
+			}
+		}
 	}
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
+	private String getFileName(final Part part) {
+		final String partHeader = part.getHeader("content-disposition");
+		// LOGGER.log(Level.INFO, "Part Header = {0}", partHeader);
+		for (String content : part.getHeader("content-disposition").split(";")) {
+			if (content.trim().startsWith("filename")) {
+				return content.substring(content.indexOf('=') + 1).trim()
+						.replace("\"", "");
+			}
+		}
+		return null;
 	}
 
 	@Override
